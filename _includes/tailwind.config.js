399871b6dd6da083691efedde486289de025200f
Jesse van der Pluijm <jessevdp@hey.com>
2020-08-13 17:27:16 +0200

    Create initial site header

--- _includes/tailwind.config.js
+++ _includes/tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
     container: {
       center: true,
     },
+    fontFamily: {
+      sans: ['"Montserrat"', 'sans-serif', '"Apple Color Emoji"', '"Segoe UI Emoji"', '"Segoe UI Symbol"', '"Noto Color Emoji"']
+    },
     extend: {},
   },
   variants: {},
