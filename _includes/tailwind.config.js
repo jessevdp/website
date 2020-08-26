62c3011cbdf182672b994ea00a9a5e00b33ff766
Jesse van der Pluijm <jessevdp@hey.com>
2020-08-26 10:08:39 +0200

    Undo auto-centering container through tailwind config

--- _includes/tailwind.config.js
+++ _includes/tailwind.config.js
@@ -1,9 +1,6 @@
 module.exports = {
   purge: [],
   theme: {
-    container: {
-      center: true,
-    },
     fontFamily: {
       sans: ['"Montserrat"', 'sans-serif', '"Apple Color Emoji"', '"Segoe UI Emoji"', '"Segoe UI Symbol"', '"Noto Color Emoji"']
     },
